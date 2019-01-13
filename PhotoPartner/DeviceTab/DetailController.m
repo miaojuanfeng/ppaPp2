19e438c0f04ac9b1a92e07e4841d6ef778afb5ec
Dreamover <dreamover@Flones-iMac.local>
2019-01-13 15:53:59 +0800

    asd

--- PhotoPartner/DeviceTab/DetailController.m
+++ PhotoPartner/DeviceTab/DetailController.m
@@ -45,11 +45,63 @@
     long offsetTop = 0;
     long messageWidth = GET_LAYOUT_WIDTH(self.scrollView)-GAP_WIDTH*4;
     float fontSize = 14;
+    UIColor *lineColor = [UIColor lightGrayColor];
+    UIView *infoView = [[UIView alloc] initWithFrame:CGRectMake(GAP_WIDTH*2, offsetTop, messageWidth, 60)];
+        UILabel *dataLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, GET_LAYOUT_WIDTH(infoView)/2, GET_LAYOUT_HEIGHT(infoView)/2)];
+        dataLabel.text = @"Wireless Data";
+        dataLabel.font = [UIFont systemFontOfSize:15];
+        [infoView addSubview:dataLabel];
+    
+        UILabel *topUpLabel = [[UILabel alloc] initWithFrame:CGRectMake(GET_LAYOUT_WIDTH(infoView)/2, 0, GET_LAYOUT_WIDTH(infoView)/2, GET_LAYOUT_HEIGHT(infoView)/2)];
+        topUpLabel.text = @"Top Up";
+        topUpLabel.textColor = [UIColor blueColor];
+        topUpLabel.font = [UIFont systemFontOfSize:15];
+        topUpLabel.textAlignment = NSTextAlignmentRight;
+        [infoView addSubview:topUpLabel];
+    
+        UILabel *totalLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, GET_LAYOUT_HEIGHT(infoView)/2-5, GET_LAYOUT_WIDTH(infoView)/2, GET_LAYOUT_HEIGHT(infoView)/2)];
+        totalLabel.font = [UIFont systemFontOfSize:fontSize];
+        totalLabel.textColor = [UIColor grayColor];
+        totalLabel.text = @"Total 10.00GB";
+        [infoView addSubview:totalLabel];
+    
+        UILabel *avaLabel = [[UILabel alloc] initWithFrame:CGRectMake(GET_LAYOUT_WIDTH(infoView)/2, GET_LAYOUT_HEIGHT(infoView)/2-5, GET_LAYOUT_WIDTH(infoView)/2, GET_LAYOUT_HEIGHT(infoView)/2)];
+        avaLabel.font = [UIFont systemFontOfSize:fontSize];
+        avaLabel.textColor = [UIColor grayColor];
+        avaLabel.textAlignment = NSTextAlignmentRight;
+        avaLabel.text = @"Available 9.66GB";
+        [infoView addSubview:avaLabel];
+    
+        UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, GET_LAYOUT_HEIGHT(infoView)-1, GET_LAYOUT_WIDTH(infoView), 1)];
+        lineView.backgroundColor = lineColor;
+        [infoView addSubview:lineView];
+    offsetTop+=GET_LAYOUT_HEIGHT(infoView);
+    self.scrollView.contentSize = CGSizeMake(GET_LAYOUT_WIDTH(self.view), offsetTop);
+    [self.scrollView addSubview:infoView];
+    UIView *usersView = [[UIView alloc] initWithFrame:CGRectMake(GAP_WIDTH*2, offsetTop, messageWidth, 36)];
+        UILabel *usersLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, GET_LAYOUT_WIDTH(usersView), GET_LAYOUT_HEIGHT(usersView))];
+        usersLabel.font = [UIFont systemFontOfSize:fontSize];
+        usersLabel.text = @"Users";
+        usersLabel.textColor = [UIColor blueColor];
+        [usersView addSubview:usersLabel];
+        UISwitch *acceptSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(GET_LAYOUT_WIDTH(usersView)-43, 5, 43, 10)];
+        acceptSwitch.transform = CGAffineTransformMakeScale(0.7, 0.7);
+        [acceptSwitch addTarget:self action:@selector(clickAcceptSwitch:) forControlEvents:UIControlEventValueChanged];
+        [usersView addSubview:acceptSwitch];
+        UILabel *acceptLabel = [[UILabel alloc] initWithFrame:CGRectMake(GET_LAYOUT_OFFSET_X(acceptSwitch)-150-GAP_WIDTH, 10, 150, 20)];
+        acceptLabel.textAlignment = NSTextAlignmentRight;
+        acceptLabel.textColor = [UIColor blueColor];
+        acceptLabel.text = @"Accept new users";
+        acceptLabel.font = [UIFont systemFontOfSize:fontSize];
+        [usersView addSubview:acceptLabel];
+    offsetTop+=GET_LAYOUT_HEIGHT(usersView);
+    self.scrollView.contentSize = CGSizeMake(GET_LAYOUT_WIDTH(self.view), offsetTop);
+    [self.scrollView addSubview:usersView];
     for(int i=0;i<20;i++){
         long messageHeight = 0;
         UIView *usersView = [[UIView alloc] initWithFrame:CGRectMake(GAP_WIDTH*2, offsetTop, messageWidth, 60)];
         UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, GET_LAYOUT_HEIGHT(usersView)-1, GET_LAYOUT_WIDTH(usersView), 1)];
-        lineView.backgroundColor = [UIColor grayColor];
+        lineView.backgroundColor = lineColor;
         [usersView addSubview:lineView];
         UIImageView *headImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 10, 40, 40)];
         headImage.layer.masksToBounds = YES;
@@ -61,14 +113,14 @@
         usernameLabel.text = @"UserName";
         usernameLabel.backgroundColor = [UIColor yellowColor];
         [usersView addSubview:usernameLabel];
-        UIButton *deleteButton = [[UIButton alloc] initWithFrame:CGRectMake(GET_LAYOUT_WIDTH(usersView)-40, 10, 40, 40)];
+        UIButton *deleteButton = [[UIButton alloc] initWithFrame:CGRectMake(GET_LAYOUT_WIDTH(usersView)-40, 15, 30, 30)];
         //deleteButton.backgroundColor = [UIColor blueColor];
         [deleteButton addTarget:self action:@selector(clickDeleteButton) forControlEvents:UIControlEventTouchUpInside];
         UIImageView *deleteImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, GET_LAYOUT_WIDTH(deleteButton), GET_LAYOUT_HEIGHT(deleteButton))];
         deleteImageView.image = [UIImage imageNamed:@"bg_main"];
         [deleteButton addSubview:deleteImageView];
         [usersView addSubview:deleteButton];
-        UIButton *settingButton = [[UIButton alloc] initWithFrame:CGRectMake(GET_LAYOUT_OFFSET_X(deleteButton)-40-GAP_WIDTH*2, 10, 40, 40)];
+        UIButton *settingButton = [[UIButton alloc] initWithFrame:CGRectMake(GET_LAYOUT_OFFSET_X(deleteButton)-30-GAP_WIDTH*2, 15, 30, 30)];
         //deleteButton.backgroundColor = [UIColor blueColor];
         [settingButton addTarget:self action:@selector(clickSettingButton) forControlEvents:UIControlEventTouchUpInside];
         UIImageView *settingImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, GET_LAYOUT_WIDTH(settingButton), GET_LAYOUT_HEIGHT(settingButton))];
@@ -92,7 +144,7 @@
         long messageHeight = 0;
         UIView *usersView = [[UIView alloc] initWithFrame:CGRectMake(GAP_WIDTH*2, offsetTop, messageWidth, 60)];
         UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, GET_LAYOUT_HEIGHT(usersView)-1, GET_LAYOUT_WIDTH(usersView), 1)];
-        lineView.backgroundColor = [UIColor grayColor];
+        lineView.backgroundColor = lineColor;
         [usersView addSubview:lineView];
         UIImageView *headImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 10, 40, 40)];
         headImage.layer.masksToBounds = YES;
@@ -119,7 +171,7 @@
         refuseButton.layer.masksToBounds = YES;
         refuseButton.layer.cornerRadius = 5;
         refuseButton.titleLabel.font = [UIFont systemFontOfSize:fontSize];
-        [refuseButton addTarget:self action:@selector(clickSettingButton) forControlEvents:UIControlEventTouchUpInside];
+        [refuseButton addTarget:self action:@selector(clickRefuseButton) forControlEvents:UIControlEventTouchUpInside];
 
         [usersView addSubview:refuseButton];
         messageHeight = GET_LAYOUT_HEIGHT(usersView);
@@ -131,12 +183,27 @@
     
 }
 
+-(void)clickAcceptSwitch:(UISwitch *)swi{
+    if(swi.isOn){
+        NSLog(@"switch on");
+    }else{
+        NSLog(@"switch off");
+    }
+}
+
 -(void)clickDeleteButton{
-    NSLog(@"hello world");
+    NSLog(@"delete");
 }
 
 -(void)clickSettingButton{
-    NSLog(@"hello world2");
+    NSLog(@"setting");
 }
 
+-(void)clickRefuseButton{
+    NSLog(@"refuse");
+}
+
+-(void)clickAcceptButton{
+    NSLog(@"accept");
+}
 @end
